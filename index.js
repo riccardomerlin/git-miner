5d18c2b27d3aa0c128a33c3b69ff1a17062728aa
Riccardo Merlin <riccardomerlin@gmail.com>
2019-06-19 14:42:18 +0100

    reorganized code

--- index.js
+++ index.js
@@ -7,49 +7,58 @@ const GitApi = require('./git-api')
 setImmediate(() => main())
 
 async function main () {
-  const git = new GitApi(argv.tab)
+  try {
+    const git = new GitApi(argv.tab)
 
-  let files = []
-  if (argv._ && argv._.length > 0) {
-    files.push(...argv._)
-  } else {
-    try {
+    let files = []
+    if (argv._ && argv._.length > 0) {
+      files.push(...argv._)
+    } else {
       files = await git.lsFiles()
-    } catch (error) {
-      exit(error.message)
     }
+
+    if (files.length === 0) {
+      exit('No files to process')
+    }
+
+    const collection = await Promise.all(files.map(async (file) => {
+      const fileRevisions = await git.log(file)
+      if (!fileRevisions) { throw new Error(`No git log for "${file}"`) }
+
+      const revisionsComplexity = await getRevisionsComplexity(fileRevisions, git)
+
+      return {
+        fileName: file,
+        revisions: revisionsComplexity
+      }
+    }))
+
+    printCsv(collection)
+  } catch (error) {
+    exit(error.message)
   }
+}
 
-  if (files.length === 0) {
-    exit('No files to process')
-  }
-
-  const collection = await Promise.all(files.map(async (file) => {
-    try {
-      const fileRevisionDateHashes = await git.log(file)
-      if (!fileRevisionDateHashes) { throw new Error(`No git log for "${file}"`) }
-
-      const revisions = await Promise.all(fileRevisionDateHashes.map(async (entry) => {
-        const blobHash = await git.lsTree(entry.gitHash, entry.fileName)
-        const complexity = await git.whitespaceComplexity(blobHash)
-
-        return `${entry.fileName},${entry.authorDate},${complexity}`
-      }))
-
-      return revisions
-    } catch (error) {
-      exit(error.message)
+async function getRevisionsComplexity (fileRevisions, git) {
+  return Promise.all(fileRevisions.map(async (fileRevision) => {
+    const revisionBlobHash = await git.lsTree(fileRevision.gitHash, fileRevision.fileName)
+    const revisionComplexity = await git.whitespaceComplexity(revisionBlobHash)
+    return {
+      date: fileRevision.authorDate,
+      complexity: revisionComplexity
     }
   }))
+}
 
+function printCsv (collection) {
   console.log('file,date,complexity')
 
   collection.forEach((entry) => {
-    if (Array.isArray(entry)) {
-      entry.forEach((item) => console.log(item))
-    } else {
-      console.log(entry)
-    }
+    if (!entry.revisions.length > 0) { return }
+
+    entry.revisions.forEach((revision) => {
+      console.log(`${entry.fileName},${revision.date},${revision.complexity}`)
+    })
   })
 }
 
